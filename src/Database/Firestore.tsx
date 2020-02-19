34ad125879e35c5cbdbcd3ea54e4c8e4beefecc6
dwestlin <dawe1103@student.miun.se>
2020-02-19 13:43:11 +0100

    Change credentials on firestore.

--- src/Database/Firestore.tsx
+++ src/Database/Firestore.tsx
@@ -1,17 +1,28 @@
 import firebase from "firebase";
 import "firebase/app";
 
-// Your web app's Firebase configuration
+//Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.FIRESTORE_API_KEY,
-  authDomain: process.env.FIRESTORE_authDomain,
-  databaseURL: process.env.FIRESTORE_databaseURL,
-  projectId: process.env.FIRESTORE_projectId,
-  storageBucket:  process.env.FIRESTORE_storageBucket,
-  messagingSenderId: process.env.FIRESTORE_messagingSenderId,
-  appId: process.env.FIRESTORE_appId,
-  measurementId: process.env.FIRESTORE_measurementId
+  apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_authDomain,
+  databaseURL: process.env.REACT_APP_FIRESTORE_databaseURL,
+  projectId: process.env.REACT_APP_FIRESTORE_projectId,
+  storageBucket:  process.env.REACT_APP_FIRESTORE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_messagingSenderId,
+  appId: process.env.REACT_APP_FIRESTORE_appId,
+  measurementId: process.env.REACT_APP_FIRESTORE_measurementId
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBo-1siFWyeZW4NeFFQpRIU5R6fQlt5BHA",
+//   authDomain: "home-website-5bc5f.firebaseapp.com",
+//   databaseURL: "https://home-website-5bc5f.firebaseio.com",
+//   projectId: "home-website-5bc5f",
+//   storageBucket: "home-website-5bc5f.appspot.com",
+//   messagingSenderId: "153385994674",
+//   appId: "1:153385994674:web:ce19c2a09382feb2e8fcab",
+//   measurementId: "G-K9Q7PSW79T"
+// };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
