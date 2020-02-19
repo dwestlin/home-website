636c59400ac336d72040dab6984104067d886786
dwestlin <dawe1103@student.miun.se>
2020-02-19 13:44:18 +0100

    once again...changed the credentials to Firestore

--- src/Database/Firestore.tsx
+++ src/Database/Firestore.tsx
@@ -13,17 +13,6 @@ var firebaseConfig = {
   measurementId: process.env.REACT_APP_FIRESTORE_measurementId
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBo-1siFWyeZW4NeFFQpRIU5R6fQlt5BHA",
-//   authDomain: "home-website-5bc5f.firebaseapp.com",
-//   databaseURL: "https://home-website-5bc5f.firebaseio.com",
-//   projectId: "home-website-5bc5f",
-//   storageBucket: "home-website-5bc5f.appspot.com",
-//   messagingSenderId: "153385994674",
-//   appId: "1:153385994674:web:ce19c2a09382feb2e8fcab",
-//   measurementId: "G-K9Q7PSW79T"
-// };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
